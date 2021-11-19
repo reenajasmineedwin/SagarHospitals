063c16ccd2b0832f76b8530f9c26a869f56473e2
sreechand2002 <sreechand2002@gmail.com>
2021-11-19 16:10:56 +0530

    backend code

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'sree');
+define('DB_NAME', 'pms');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+?>
