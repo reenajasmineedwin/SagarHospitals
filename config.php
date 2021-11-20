389e61d1fe6495a09adc36950c0c834eb020b289
Abhishek Samuel Edwin <abhishek@Abhisheks-MacBook-Pro.local>
2021-11-20 20:17:14 +0530

    updated backend

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS' ,'sree');
-define('DB_NAME', 'pms');
+define('DB_PASS' ,'');
+define('DB_NAME', 'InpatientFeedback');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
