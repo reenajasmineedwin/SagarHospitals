581e8e6608b0036ba6cdacf1de49b1aa1ceef170
Reena Jasmine Edwin <76644184+reenajasmineedwin@users.noreply.github.com>
2021-11-30 00:35:22 +0530

    Add files via upload

---
+++ inpatient_get-subcat_branch.php
@@ -0,0 +1,13 @@
+<?php
+include "inconfig.php";
+$category_id = $_POST["category_id"];
+$result = mysqli_query($con, "SELECT * FROM Doctor_Data where parent_id = $category_id");
+?>
+<option value="">Select Specialty</option>
+<?php
+while ($row = mysqli_fetch_array($result)) {
+    ?>
+    <option value="<?php echo $row["id"]; ?>"><?php echo $row["category"]; ?></option>
+    <?php
+}
+?>
